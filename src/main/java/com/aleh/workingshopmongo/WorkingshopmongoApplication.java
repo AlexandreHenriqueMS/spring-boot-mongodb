6a7e8490d43e5290715e0f62eb9f1d13e0335ad4
Alexandre <Alexandre_henrique98@hotmail.com>
2019-11-22 07:45:55 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/aleh/workingshopmongo/WorkingshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.aleh.workingshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingshopmongoApplication.class, args);
+	}
+
+}
