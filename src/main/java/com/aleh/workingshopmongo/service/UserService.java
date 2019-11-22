22924456967a4d1b0d4b2de0cf8ac590591f9872
Alexandre <Alexandre_henrique98@hotmail.com>
2019-11-22 10:10:16 -0300

    Tratamento de exceção 404

--- src/main/java/com/aleh/workingshopmongo/service/UserService.java
+++ src/main/java/com/aleh/workingshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.aleh.workingshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aleh.workingshopmongo.domain.User;
 import com.aleh.workingshopmongo.repository.UserRepository;
+import com.aleh.workingshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User FindById(String id){
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
